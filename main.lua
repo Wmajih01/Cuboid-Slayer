99f184a87d55471693d2b17cd675a95e76eba999
alexite03 <alexander.bykadorov@skola.botkyrka.se>
2021-02-22 16:57:30 +0100

    experimenterar med world och fysikaliska funktioner

--- main.lua
+++ main.lua
@@ -1,3 +1,18 @@
+local world = love.physics.newWorld(0, 100)
+
+local circleEntity = {}
+
+circleEntity.body = love.physics.newBody(world, 200, 200, "dynamic")
+circleEntity.body:setMass(32)
+circleEntity.shape = love.physics.newCircleShape(50)
+circleEntity.fixture = love.physics.newFixture(circleEntity.body, circleEntity.shape)
+
+love.update = function(dt)
+    world:update(dt)
+end
+
+print(circleEntity.body:getWorldPoint(circleEntity.shape:getPoint()))
+
 love.draw = function ()
-    love.graphics
+    love.graphics.circle("fill", circleEntity.body:getWorldPoint())
 end
